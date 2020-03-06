1efe5294e9d69c1788042ffc7fd36960640ee27b
Do Chien Thang <thangdc1910@gmail.com>
2020-03-07 02:25:43 +0800

    update BaTest.java

--- src/BaTest.java
+++ src/BaTest.java
@@ -1,6 +1,8 @@
 public class BaTest {
+  public static void main(String[] args) {
+    System.out.println("XXXXXxxxx");
+  }
   public static void sayHi(){
-    System.out.println("444");
     System.out.println("555");
     System.out.println("666");
   }
