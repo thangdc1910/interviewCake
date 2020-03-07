86fbfb5c5ec69e139a3ffee492a2e4f22e4d923a
Do Chien Thang <thangdc1910@gmail.com>
2020-03-07 17:41:15 +0800

    update

---
+++ src/Camel.java
@@ -0,0 +1,9 @@
+public class Camel {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<10){
+            System.out.println("Hello Camel");
+            i++;
+        }
+    }
+}
