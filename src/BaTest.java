4bfed157bdbb7553c6a676d0f1e1e0923c5dbd66
Do Chien Thang <thangdc1910@gmail.com>
2020-03-07 11:58:51 +0800

    final update

--- src/BaTest.java
+++ src/BaTest.java
@@ -2,6 +2,8 @@ public class BaTest {
   public static void main(String[] args) {
     System.out.println("XXXXXxxxx");
     System.out.println("hello corona");
+    System.out.println("hello Michael batoac");
+    System.out.println("Test Git 123456");
   }
   public static void sayHi(){
     System.out.println("555");
